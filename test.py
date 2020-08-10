2792e23dccd890df3d144c1b45e443ce05f95744
Daniel <daniel@funmobig.com>
2020-08-10 13:42:19 +0800

    [A]添加忽略文件

---
+++ test.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# 主入口
+
+import controllers.controller as controllers
+
+if __name__ == '__main__':
+       result = controllers.handText("获取ip")
+      #  print('result:',result)
+      #  result = controllers.handText("执行任务:name=_startToutiaoLottery2",MANAGER)
+    #    result = controllers.handText("获取用户信息:id=omyqB1uI5qSm5Ypdum43V2zMrTVk",MANAGER)
+       print('result',result)
+      # autoFollow()
+    # task.startTasks()
+    # print(task.getJobs())
